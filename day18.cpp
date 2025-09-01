0a0ab06bce5dac57bafa8e85a3519250506c92fb
maheshtamnar <mahesh.tamnar24@vit.edu>
2025-09-01 20:51:17 +0530

    Create day18.cpp

---
+++ day18.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int countDivisors(long long n) {
+    int count = 0;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            if (i == n / i)
+                count++;
+            else
+                count += 2;
+        }
+    }
+    return count;
+}
+
+int main() {
+    long long N;
+    cin >> N;
+    cout << countDivisors(N) << endl;
+    return 0;
+}
